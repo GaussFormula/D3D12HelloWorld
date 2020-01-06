0c77ffcf3a1418520e536050cb08db8d874daa17
GaussFormula <c594794@hotmail.com>
2020-01-06 22:14:09 +0800

    添加项目文件。

---
+++ D3D12HelloWorld/DXSample.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "DXSample.h"
+
+using namespace Microsoft::WRL;
+
+DXSample::DXSample(UINT width, UINT height, std::wstring name) :
+    m_width(width),
+    m_height(height),
+    m_title(name),
+    m_useWarpDevice(false)
+{
+    WCHAR assetsPath[512];
+    GetAssetsPath(assetsPath, _countof(assetsPath));
+    m_assetsPath = assetsPath;
+
+    m_aspectRatio = static_cast<float>(width) / static_cast<float>(height);
+}
+
+DXSample::~DXSample()
+{
+
+}
+
+//Helper function for resolving the full path of assets.
+std::wstring DXSample::GetAssetFullPath(LPCWSTR assetName)
+{
+    return m_assetsPath + assetName;
+}
