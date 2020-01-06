0e0c164e23087606a9100d516e1ec0ff819c9c19
GaussFormula <c594794@hotmail.com>
2020-01-07 01:31:37 +0800

    Add DXSample::ParseCommandLineArgs

--- D3D12HelloWorld/DXSample.cpp
+++ D3D12HelloWorld/DXSample.cpp
@@ -54,3 +54,25 @@ void DXSample::GetHardwareAdapter(IDXGIFactory2* pFactory, IDXGIAdapter1** ppAda
     }
     *ppAdapter = adapter.Detach();
 }
+
+// Helper function for setting the window's title text.
+void DXSample::SetCustomWindowText(LPCWSTR text)
+{
+    std::wstring windowText = m_title + L": " + text;
+    SetWindowText(Win32Application::GetHwnd(), windowText.c_str());
+}
+
+// Helper function for parsing any supplied command line args.
+_Use_decl_annotations_
+void DXSample::ParseCommandLineArgs(_In_reads_(argc) WCHAR* argv[], int argc)
+{
+    for (int i=1;i<argc;++i)
+    {
+        if (_wcsnicmp(argv[i], L"-warp", wcslen(argv[i])) == 0 ||
+            _wcsnicmp(argv[i], L"/warp", wcslen(argv[i])) == 0)
+        {
+            m_useWarpDevice = true;
+            m_title = m_title + L" (WARP)";
+        }
+    }
+}
