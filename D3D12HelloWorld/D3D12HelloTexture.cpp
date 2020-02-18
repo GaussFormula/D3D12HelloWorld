41784c438430ba98a6e370a8a3affe3f165e60b1
GaussFormula <c594794@hotmail.com>
2020-02-19 00:47:08 +0800

    Add RTV heap

--- D3D12HelloWorld/D3D12HelloTexture.cpp
+++ D3D12HelloWorld/D3D12HelloTexture.cpp
@@ -55,17 +55,50 @@ void D3D12HelloTexture::LoadPipeline()
         ThrowIfFailed(D3D12CreateDevice(hardwareAdapter.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&m_device)));
     }
 
-    // Descirbe and create the command queue.
+    // Describe and create the command queue.
     D3D12_COMMAND_QUEUE_DESC queueDesc = {};
     queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
     queueDesc.Type = D3D12HelloTexture::CommandListType;
     ThrowIfFailed(m_device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&m_commandQueue)));
 
     // Create command allocator.
-    ThrowIfFailed(m_device->CreateCommandAllocator(D3D12HelloTexture::CommandListType, IID_PPV_ARGS(&m_commandAllocator));
+    ThrowIfFailed(m_device->CreateCommandAllocator(D3D12HelloTexture::CommandListType, IID_PPV_ARGS(&m_commandAllocator)));
 
     // Describe and create the swap chain.
     DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
     swapChainDesc.BufferCount = FrameCount;
+    swapChainDesc.Width = m_width;
+    swapChainDesc.Height = m_height;
+    swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+    swapChainDesc.SampleDesc.Count = 1;
 
+    ComPtr<IDXGISwapChain1> swapChain;
+    ThrowIfFailed(factory->CreateSwapChainForHwnd(
+        m_commandQueue.Get(),   // Swap chain needs the queue so that it can force a flush on it.
+        Win32Application::GetHwnd(),
+        &swapChainDesc,
+        nullptr,
+        nullptr,
+        &swapChain
+    ));
+
+    // This sample does not support fullscreen transitions.
+    ThrowIfFailed(factory->MakeWindowAssociation(Win32Application::GetHwnd(), DXGI_MWA_NO_ALT_ENTER));
+
+    ThrowIfFailed(swapChain.As(&m_swapChain));
+
+    // Initialize m_frameIndex
+    m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
+
+    // Create descriptor heaps.
+    {
+        // Describe and create a render target view descriptor
+        D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+        rtvHeapDesc.NumDescriptors = FrameCount;
+        rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+        rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+        ThrowIfFailed(m_device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&m_rtvHeap)));
+    }
 }
