e47da7db0b5b9cbcd842e2e11fec6225f638b2f9
翊岚 邓 <c594794@hotmail.com>
2020-04-24 19:04:25 +0800

    Update something

--- D3D12HelloWorld/D3D12HelloWindow.cpp
+++ D3D12HelloWorld/D3D12HelloWindow.cpp
@@ -1,6 +1,12 @@
 #include "stdafx.h"
 #include "D3D12HelloWindow.h"
 
+struct Vertex
+{
+    XMFLOAT3 Pos;
+    XMFLOAT4 Color;
+};
+
 D3D12HelloWindow::D3D12HelloWindow(UINT width, UINT height, std::wstring name,UINT frameCount=2):
     DXSample(width,height,name,frameCount)
 {
@@ -303,4 +309,45 @@ void D3D12HelloWindow::BuildShaderAndInputLayout()
     ThrowIfFailed(D3DCompileFromFile(GetAssetFullPath(L"shaders.hlsl").c_str(), nullptr, nullptr, "PSMain", "ps5_0", compileFlags, 0, &m_psByteCode, nullptr));
     m_inputLayout.push_back(InitInputLayoutDescription((LPSTR)"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0));
     m_inputLayout.push_back(InitInputLayoutDescription((LPSTR)"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0));
+}
+
+void D3D12HelloWindow::BuildOwnGeometry()
+{
+    std::array<Vertex, 8> vertices =
+    {
+        Vertex({ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT4(Colors::White) }),
+        Vertex({ XMFLOAT3(-1.0f, +1.0f, -1.0f), XMFLOAT4(Colors::Black) }),
+        Vertex({ XMFLOAT3(+1.0f, +1.0f, -1.0f), XMFLOAT4(Colors::Red) }),
+        Vertex({ XMFLOAT3(+1.0f, -1.0f, -1.0f), XMFLOAT4(Colors::Green) }),
+        Vertex({ XMFLOAT3(-1.0f, -1.0f, +1.0f), XMFLOAT4(Colors::Blue) }),
+        Vertex({ XMFLOAT3(-1.0f, +1.0f, +1.0f), XMFLOAT4(Colors::Yellow) }),
+        Vertex({ XMFLOAT3(+1.0f, +1.0f, +1.0f), XMFLOAT4(Colors::Cyan) }),
+        Vertex({ XMFLOAT3(+1.0f, -1.0f, +1.0f), XMFLOAT4(Colors::Magenta) })
+    };
+    std::array<std::uint16_t, 36> indices =
+    {
+        // front face
+        0,1,2,
+        0,2,3,
+
+        // back face
+        4, 6, 5,
+        4, 7, 6,
+
+        // left face
+        4, 5, 1,
+        4, 1, 0,
+
+        // right face
+        3, 2, 6,
+        3, 6, 7,
+
+        // top face
+        1, 5, 6,
+        1, 6, 2,
+
+        // bottom face
+        4, 0, 3,
+        4, 3, 7
+    };
 }
